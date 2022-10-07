e358a4f3e8e3cb5df4126dcbe1103e7592631ffa
Vivek Nair <vnair611@gmail.com>
2022-10-07 14:20:18 -0400

    Add in the animation between background-colors states

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Vivek Nair";
-export const SITE_DESCRIPTION = "Purveyor of 203 status responses";
+export const SITE_DESCRIPTION = "Finest purveyor of 203 status responses";
